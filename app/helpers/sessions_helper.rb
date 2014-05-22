8d0bc6b2b2a8521baa36535b3f95f3616a99c323
Etienne <vivien.etienne@gmail.com>
2014-05-21 19:12:33 -0700

    Friendly Forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,11 +22,20 @@ module SessionsHelper
   def current_user?(user)
     user == current_user
   end
-  
+
   def sign_out
     current_user.update_attribute(:remember_token,
                                   User.digest(User.new_remember_token))
     cookies.delete(:remember_token)
     self.current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
   end
 end
